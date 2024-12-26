467b97230ff04399a96a717c983c62481cf6ba98
Mike <michaelaugusta15@gmail.com>
2024-12-26 14:31:52 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module SECURE_LOGIN
+
+go 1.23.2
+
+require golang.org/x/crypto v0.31.0
---
+++ main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Login struct {
+	HashedPassword string
+	SessionToken   string
+	CSRFToken      string
+}
+
+// Key is the username
+var users = map[string]Login{}
+
+func main() {
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/protected", protected)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		er := http.StatusMethodNotAllowed
+		http.Error(w, "Invalid method", er)
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	if len(username) < 8 || len(password) < 8 {
+		er := http.StatusNotAcceptable
+		http.Error(w, "Invalid username/password", er)
+		return
+	}
+
+	if _, ok := users[username]; ok {
+		er := http.StatusConflict
+		http.Error(w, "User already exists", er)
+		return
+	}
+
+	hashedPassword, _ := hashPassword(password)
+	users[username] = Login{
+		HashedPassword: hashedPassword,
+	}
+
+	fmt.Fprintln(w, "User registrered successfully!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	// Placeholder for login logic
+
+	if r.Method != http.MethodPost {
+		er := http.StatusMethodNotAllowed
+		http.Error(w, "Invalid request method", er)
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	user, ok := users[username]
+	if !ok || !checkPasswordHash(password, user.HashedPassword) {
+		er := http.StatusUnauthorized
+		http.Error(w, "Invalid username or password", er)
+		return
+	}
+
+	sessionToken := generateToken(32)
+	csrfToken := generateToken(32)
+
+	//Set seesion Cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    sessionToken,
+		Expires:  time.Now().Add(24 * time.Hour),
+		HttpOnly: true,
+	})
+
+	//Set CSRF token in a cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:     "csrf_token",
+		Value:    csrfToken,
+		Expires:  time.Now().Add(24 * time.Hour),
+		HttpOnly: false, //need to be accesible from the client-side
+	})
+
+	//Store tokens in the database
+	user.SessionToken = sessionToken
+	user.CSRFToken = csrfToken
+	users[username] = user
+
+	fmt.Fprintln(w, "Login successful!")
+}
+
+func protected(w http.ResponseWriter, r *http.Request) {
+	// Placeholder for protected endpoint logic
+	if r.Method != http.MethodPost {
+		er := http.StatusMethodNotAllowed
+		http.Error(w, "Invalid request method", er)
+		return
+	}
+
+	if err := Authorize(r); err != nil {
+		er := http.StatusUnauthorized
+		http.Error(w, "Unauthorized protected", er)
+		return
+	}
+
+	username := r.FormValue("username")
+	fmt.Fprintf(w, "CSRF validation successful! Welcome, %s", username)
+
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	// Placeholder for logout logic
+	if err := Authorize(r); err != nil {
+		er := http.StatusUnauthorized
+		http.Error(w, "Unauthorized logout", er)
+		return
+	}
+
+	//Clear Cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HttpOnly: true,
+	})
+
+	//Set CSRF token in a cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:     "csrf_token",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HttpOnly: false, //need to be accesible from the client-side
+	})
+
+	//Clear the tokens from the database
+	username := r.FormValue("username")
+	user, _ := users[username]
+	user.SessionToken = ""
+	user.CSRFToken = ""
+	users[username] = user
+
+	fmt.Fprintln(w, "Logged out successfully!")
+}
